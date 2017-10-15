350c24632e81b77bb39cead7aea99e020e350edb
Roberta Nin <ninfeliz@seas.upenn.edu>
2017-10-15 15:16:57 -0400

    Referral

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import Form
+from wtforms import ValidationError
+from wtforms.fields import StringField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import Email, InputRequired, Length
+
+from ..models import User
+
+
+class ReferUserForm(Form):
+    first_name = StringField(
+        'First name', validators=[InputRequired(), Length(1, 64)])
+    last_name = StringField(
+        'Last name', validators=[InputRequired(), Length(1, 64)])
+    email = EmailField(
+        'Email', validators=[InputRequired(), Length(1, 64), Email()])
+    submit = SubmitField('Invite')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
-from flask import render_template
-from ..models import EditableHTML
+from flask import render_template, url_for, flash
+from flask_rq import get_queue
+from ..models import EditableHTML, User
 
 from . import main
+from .forms import ReferUserForm
+from .. import db
+from ..email import send_email
 
 
 @main.route('/')
@@ -14,3 +18,33 @@ def about():
     editable_html_obj = EditableHTML.get_editable_html('about')
     return render_template('main/about.html',
                            editable_html_obj=editable_html_obj)
+
+
+@main.route('/referral')
+def refer_user():
+    """Invites a new user to create an account and set their own password."""
+    form = ReferUserForm()
+    if form.validate_on_submit():
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        invite_link = url_for(
+            'account.join_from_invite',
+            user_id=user.id,
+            token=token,
+            _external=True)
+        get_queue().enqueue(
+            send_email,
+            recipient=user.email,
+            subject='You Are Invited To Join',
+            template='account/email/invite',
+            user=user,
+            invite_link=invite_link, )
+        flash('User {} successfully invited'.format(user.full_name()),
+              'form-success')
+    return render_template('main/referral.html', form=form)
+
