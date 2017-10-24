bc41a9669727e836a7e61812b02b1ee3b45d51cd
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2017-10-24 13:00:54 -0400

    display form to user, get responses on submit, and save to db

--- app/models/forms.py
+++ app/models/forms.py
@@ -1,5 +1,5 @@
 from .. import db
-
+from . import User
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,4 +7,12 @@ class Form(db.Model):
 
     @staticmethod
     def get_form_content():
-        return Form.query.order_by('id desc').first()
+        return Form.query.order_by('id desc').first().content
+
+class FormResponse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.PickleType, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    def __init__(self, user_id, content):
+        self.content = content
+        self.user_id = user_id
