3cd1eb2774b4a3a53328088e762e5d9e02232758
Nishita Jain <jain.nishita@hotmail.com>
2017-11-05 15:32:13 -0500

    teammembers database

--- app/models/teams.py
+++ app/models/teams.py
@@ -4,6 +4,15 @@ from .. import db, login_manager
 class Team(db.Model):
     __tablename__ = 'team'
     id = db.Column(db.Integer, primary_key=True)
-    owner = db.Column(db.Integer, db.ForeignKey('users.id'))
-    members = db.relationship('User', db.backref('team'))
+    # owner = db.Column(db.Integer, db.ForeignKey('users.id'))
+    members = db.relationship('TeamMember', backref='team')
     is_confirmed = db.Column(db.Boolean)
+
+
+class TeamMember(db.Model):
+    __tablename = 'teammember'
+    id = db.Column(db.Integer, primary_key=True)
+    is_confirmed = db.Boolean
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref=db.backref('teammember', uselist=False))
--- app/models/user.py
+++ app/models/user.py
@@ -71,7 +71,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    # owner_team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     # team = db.relationship('team', back_populates='members')
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     team_confirmed = db.Boolean
 
     referrers = db.relationship("User", secondary=referrals,
