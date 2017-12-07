d1536ce82388a1c2061fe58aa6f980348a5fe0e8
Santiago Buenahora <santib97@gmail.com>
2017-12-06 23:40:12 -0500

    Add flow logic w signup, login

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,59 @@
-from flask import render_template, request, jsonify, abort, url_for, redirect
+from flask import flash, render_template, request, jsonify, abort, url_for, redirect
+from flask_rq import get_queue
+from flask_login import current_user, login_user
 
 import jsonpickle
-from ..models import EditableHTML, Form, FormResponse, Role, User
-from .. import db, csrf
-from . import main
-from flask_login import current_user
 
-@main.route('/')
+from .. import db, csrf
+from ..models import EditableHTML, Form, FormResponse, Role, User
+from ..email import send_email
+from . import main
+from .forms import LoginForm, RegistrationForm
+
+
+@main.route('/', methods=['GET', 'POST'])
 def index():
+    # Log in an existing user.
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.password_hash is not None and \
+                user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember_me.data)
+            flash('You are now logged in. Welcome back!', 'success')
+            return redirect(request.args.get('next') or url_for('main.index'))
+        else:
+            flash('Invalid email or password.', 'form-error')
+
+    # Register a new user, and send them a confirmation email.
+    registration_form = RegistrationForm()
+    if registration_form.validate_on_submit():
+        user = User(
+            first_name=registration_form.first_name.data,
+            last_name=registration_form.last_name.data,
+            email=registration_form.email.data,
+            password=registration_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        confirm_link = url_for('account.confirm', token=token, _external=True)
+        get_queue().enqueue(
+            send_email,
+            recipient=user.email,
+            subject='Confirm Your Account',
+            template='account/email/confirm',
+            user=user,
+            confirm_link=confirm_link)
+        flash('A confirmation link has been sent to {}.'.format(user.email),
+              'warning')
+        return redirect(url_for('main.index'))
+
+    return render_template('main/index.html',
+                           registration_form=registration_form, login_form=login_form)
+
+
+@main.route('/form')
+def signup():
     content = None
     form_resp_obj = []
     try:
@@ -32,7 +78,7 @@ def index():
                 form_resp_obj = sorted(form_resp_obj, key=lambda k: k['idx'])
     except Exception:
         pass
-    return render_template('main/index.html', content=content, form_resp_obj=form_resp_obj)
+    return render_template('main/form.html', content=content, form_resp_obj=form_resp_obj)
 
 
 @main.route('/edit-form')
